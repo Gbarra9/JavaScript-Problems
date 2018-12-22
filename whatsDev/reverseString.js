1dc25ef16646b1898be701bb22395bb8c4da2c73
Gbarra9 <gebarra9@gmail.com>
2018-12-21 20:13:50 -0500

    reverse string

--- whatsDev/reverseString.js
+++ whatsDev/reverseString.js
@@ -1,1 +1,41 @@
-test
+// REVERSE METHOD (exclusive method for array)
+
+
+function reverseString(str) {
+    // split string into array without spaces, each letter separated by comma
+    // first items becomes last, last item becomes first
+    // join method('') removes commma, each item in array joined together and converted into string
+    return str.split('').reverse().join('')
+}
+
+console.log(reverseString('hello'))
+
+// FOR LOOP INCREMENT
+
+function reverseString(str) {
+    // create var to store letters after each loop
+    // .length access a letter in a string by position
+    let string = ''
+    for (let i = 0; i <= str.length - 1; i++) {
+        string = str[i] + string
+    }
+    return string
+}
+
+console.log(reverseString('hello'))
+
+
+// FOR LOOP DECREMENT
+
+function reverseString(str) {
+    // create var to store letters after each loop
+    // .length access a letter in a string by position
+    let string = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        string += str[i]
+    }
+    return string
+
+}
+
+console.log(reverseString('hello'))
